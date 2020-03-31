98208b6f014662e61a1ce03f4851b1df05568f54
jeppe sinkbaek <jeppest2@gmail.com>
2020-03-31 18:14:24 +0200

    bugfixing

--- assignment1/main.py
+++ assignment1/main.py
@@ -6,22 +6,32 @@ from astropy.table import Table, join, vstack, unique, setdiff, Column
 from astropy import units as un
 import astropy.coordinates as coord
 import time as tm
+import seaborn as sns
 
 path_to_data = '/data/astronomy/gaia/dr2/'
 fnames = []
 with open(path_to_data+'fnames', 'r') as f:
+    j = 0
     for line in f:
         fnames.append(line.split())
+        j += 1
+        if j == 8:
+            break
 # fnames.append(['GaiaSource_6714230465835878784_6917528443525529728.csv'])
 
 
 i = 0
 lfnames = len(fnames)
+
+
 for name in fnames:
+    print('i = ', i)
+
     # Read a data file
     F = Table.read(path_to_data+name[0], include_names=['parallax', 'ra', 'dec', 'pmra',
                                                         'pmdec', 'radial_velocity'],
-                   format='csv')
+                   format='csv', guess=False, fast_reader={'chunk_size': 500 * 1000000})
+
     if i == 0:
         # Initialize arrays
         par = np.ma.empty((F['parallax'].size, lfnames))
@@ -30,6 +40,13 @@ for name in fnames:
         pmra = np.ma.empty((F['pmra'].size, lfnames))
         pmdec = np.ma.empty((F['pmdec'].size, lfnames))
         rvel = np.ma.empty((F['radial_velocity'].size, lfnames))
+
+        par[:] = np.nan
+        ra[:] = np.nan
+        dec[:] = np.nan
+        pmra[:] = np.nan
+        pmdec[:] = np.nan
+        rvel[:] = np.nan
 
     else:
         # Check if array lengthening must be implemented to accomodate new data
@@ -64,12 +81,18 @@ for name in fnames:
     pmdec[0:F['pmdec'].size, i] = F['pmdec']
     rvel[0:F['radial_velocity'].size, i] = F['radial_velocity']
 
+    # Convert to arrays filled wtih nan values instead of masked array
+    par = np.ma.filled(par, fill_value=np.nan)
+    ra = np.ma.filled(ra, fill_value=np.nan)
+    dec = np.ma.filled(dec, fill_value=np.nan)
+    pmra = np.ma.filled(pmra, fill_value=np.nan)
+    pmdec = np.ma.filled(pmdec, fill_value=np.nan)
+    rvel = np.ma.filled(rvel, fill_value=np.nan)
+
     print(name[0])
     print(F.info)
     del F
-    print(par.shape)
     i += 1
-    print('i = ', i)
 
 # # Solar parameters wrt Galaxy, LSR and galactic plane
 X_GC_sun = 8 * un.kpc
@@ -87,13 +110,13 @@ vZ_GC_sun = W_LSR
 # # Coordinate units input, see units.pdf for reference
 ra = ra * un.degree
 dec = dec * un.degree
-dist = 1/np.ma.abs(par) * un.parsec
+dist = 1000/np.abs(par) * un.pc
 pmra = pmra * un.mas/un.year
 pmdec = pmdec * un.mas/un.year
 rvel = rvel * un.km/un.s
 
 # # ICRS objects
-icrs = coord.ICRS(ra=ra, dec=dec, distance=dist, pm_ra_cosdec=pmra, pm_dec=pmdec,
+icrs = coord.ICRS(ra=ra, dec=dec, distance=dist/1000, pm_ra_cosdec=pmra, pm_dec=pmdec,
                   radial_velocity=rvel)
 
 # Galactocentric frame
@@ -103,14 +126,93 @@ gc = coord.Galactocentric(galcen_distance=X_GC_sun, galcen_v_sun=
 
 galcen = icrs.transform_to(gc)
 
+# LSR frame
+lsr = icrs.transform_to(coord.LSR)
+
 # Stars GC cartesian coordinates
 x = galcen.x.to(un.kpc).value   # in kpc
 y = galcen.y.to(un.kpc).value   # in kpc
+z = galcen.z.to(un.kpc).value   # in kpc
+d_gc = np.sqrt(x**2 + y**2 + z**2)  # distance from GC in kpc
 
-plt.plot(x, y, '.')
+# Stars LSR velocities less than 100pc
+dist_lsr = dist  # lsr.distance.to(un.pc).value  # in pc, unnecessary since LSR transform does not change coordinate
+# reference point
+arg_100pc = np.where(dist_lsr < 100)
+arg_100pc_extra = np.where(dist.value < 100)
+U = lsr.radial_velocity.to(un.km/un.s).value    #
+pmra_lsr = lsr.pm_ra_cosdec.to(un.mas/un.yr).value
+pmdec_lsr = lsr.pm_dec.to(un.mas/un.yr).value
+pm_lsr = [pmra_lsr, pmdec_lsr] * un.mas/un.yr
+V, W = (pm_lsr * dist_lsr*un.pc).to(un.km/un.s, un.dimensionless_angles())
+
+dist_100pc = dist_lsr[arg_100pc]
+dist_100pc_extra = dist[arg_100pc_extra]
+print('dist_lsr.size', dist_lsr.size)
+print('dist_100pc.size', dist_100pc.size)
+print('extra', dist_100pc_extra.size)
+U_100pc = U[arg_100pc]
+V_100pc = V[arg_100pc].value
+W_100pc = W[arg_100pc].value
+
+print(V_100pc.dtype)
+
+# # Scheming
+plt.figure()
+plt.plot(np.sort(dist[dist.value/1000 < 100].ravel() / 1000), 'r*')
+
+plt.figure()
+plt.plot(x, y, '.', markersize=2)
+# sns.set_palette(sns.color_palette('hls', 8))
 plt.plot(-X_GC_sun, 0, 'b*', markersize=8)
-plt.xlim([-20, 15])
+plt.xlim([-12.5, 7.5])
 plt.ylim([-20, 20])
-plt.xlabel('kpc')
-plt.ylabel('kpc')
+plt.xlabel('X kpc')
+plt.ylabel('Y kpc')
 plt.show()
+
+plt.figure()
+plt.plot(x, z, '.', markersize=2)
+plt.xlabel('X kpc')
+plt.ylabel('Z kpc')
+# plt.show(block=False)
+
+plt.figure()
+plt.plot(x, d_gc, 'b.', markersize=2)
+plt.plot(y, d_gc, 'r.', markersize=2)
+plt.plot(z, d_gc, 'y.', markersize=2)
+plt.xlabel('Galactocentric carthesian coordinate in kpc')
+plt.ylabel('Distance from galactic centre in kpc')
+#plt.xlim([-75, 75])
+#plt.ylim([-5, 100])
+
+# plt.figure()
+# plt.plot(U_100pc, dist_100pc, '*', markersize=2)
+# plt.ylabel('Distance from LSR in parsec')
+# plt.xlabel('Velocity component in km/s')
+
+# plt.figure()
+# plt.plot(V_100pc, dist_100pc, '*', markersize=2)
+
+# plt.figure()
+# plt.plot(W_100pc, dist_100pc, '*', markersize=2)
+
+# Histogram LSR velocities less than 100pc
+nbins = 50
+u100 = np.copy(U_100pc)
+v100 = np.copy(V_100pc)
+w100 = np.copy(W_100pc)
+
+plt.figure()
+#plt.hist(u100, bins=nbins)
+
+plt.figure()
+
+#plt.hist(v100, bins=nbins)
+
+plt.figure()
+
+#plt.hist(w100, bins=nbins)
+
+
+
